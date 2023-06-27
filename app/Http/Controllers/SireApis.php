a4b71d013d9ca824e7e091f23c18720f84143d57
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-06-27 10:59:12 -0600

    Update SireApis.php
    se actualiza servicio SIRE Consulta Anual de Presupuesto

--- app/Http/Controllers/SireApis.php
+++ app/Http/Controllers/SireApis.php
@@ -7,6 +7,8 @@ use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request as Psr7Request;
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\Print_;
+
 class SireApis extends Controller {
 
     use ApiKeyTrait;
@@ -194,12 +196,21 @@ public function ConsultaPresupuesto(Request $request){
 
 }
 
+
+
 public function ConsultaPresupuestoAnual(Request $request){
+
+
   $SUCCESS = true;
   $NUMCODE = 0;
   $STRMESSAGE = 'Exito';
   $response = "";
+  $responses= "";
   try {
+
+    
+
+
       if($request->anio ===""){
           throw new Exception("El Parámetro anio es Obligatorio");
       }
@@ -210,105 +221,162 @@ public function ConsultaPresupuestoAnual(Request $request){
       $private =  env('APP_SIRE_API_SECRET');
       $firma =  $this->generaHMAC($public,$private,'RConsultaClavesPresupuestales');
 
-//Se crea un array llamado $momentosContables que contiene todos los momentos contables requeridos.
-      $momentosContables = [
-        "Estimado",
-        "Aprobado",
-        "Ampliacion",
-        "Reduccion",
-        "Transferencia_Aumento",
-        "Transferencia_Reduccion",
-        "Saldo",
-        "PreComprometer",
-        "Disponible",
-        "Comprometido",
-        "PreComprometido_Sin_Comprometer",
-        "Para_PreComprometer",
-        "Devengado",
-        "No_Devengado",
-        "PreComprometido_Sin_Deven",
-        "Ejercido",
-        "Recaudado",
-        "Devengado_Sin_Ejerc",
-        "Pagado",
-        "Ejercido_Sin_Pagar",
-        "PorPagar"
-    ];
-    
-//Se crea un array $momentos que utiliza los momentos contables como claves y establece el valor de cada momento en "S".
-    $momentos = [];
-    foreach ($momentosContables as $momento) {
-        $momentos[$momento] = "S";
-        if($momento == $momentos){
-print('ultimo');
-        }
-      //--  print($momentos);
-    }
-//Dentro del cuerpo de la solicitud JSON, se utiliza json_encode() para convertir el array $momentos en un objeto JSON válido.
-    $body = '{
-        "Input": {
-          "Request": {
-            "Acceso": {
-              "ApiPublic":"'.$public.'",
-              "Firma":"'.$firma.'",
-              "Fecha":"'.$date.'"
-            },
-            "ConsultaDatosClaves": {
-              "TipoCvePresupuestal": "'.env('APP_SIRE_API_EJERCICIO').'" ,
-              "Periodo": '.$request->anio.',
-              "CargarSaldos": "S",
-              "CodigosClasificadores": {
-                "Clasificador1": "'.  $request->clasificador1.'",
-                "Clasificador2": "'.  $request->clasificador2.'",
-                "Clasificador3": "'.  $request->clasificador3.'",
-                "Clasificador4": "'.  $request->clasificador4.'",
-                "Clasificador5": "'.  $request->clasificador5.'",
-                "Clasificador6": "'.  $request->clasificador6.'",
-                "Clasificador7": "'.  $request->clasificador7.'",
-                "Clasificador8": "'.  $request->clasificador8.'",
-                "Clasificador9": "'.  $request->clasificador9.'",
-                "Clasificador10":"'.  $request->clasificador10.'",
-                "Clasificador11":"'.  $request->clasificador11.'"
-              },
-              "MomentosContables": '.json_encode($momentos).'
-              
-            }
-          }
-        }
-      }';
-      print(  json_last_error_msg());
-       print(    $body );
-      $client = new Client();
-      $headers = ['Content-Type' => 'application/json'];
-      $req = new Psr7Request('POST', env('APP_SIRE_URL').'/apirest/catalogos/RConsultaClavesPresupuestales', $headers, $body);
-      $res = $client->sendAsync($req)->wait();
-      $data = json_decode($res->getBody()->getContents());
+      $enero       ='"S"';
+      $febrero     ='"S"';
+      $marzo       ='"S"';
+      $abril       ='"S"';
+      $mayo        ='"S"';
+      $junio       ='"S"';
+      $julio       ='"S"';
+      $agosto      ='"S"';
+      $septiembre  ='"S"';
+      $octubre     ='"S"';
+      $noviembre   ='"S"';
+      $diciembre   ='"S"';
 
-      if ($data->Result->Response->Error) {
-          throw new Exception($data->Result->Response->Error);
-      } else {
-          $response = $data->Result->Response->Claves->Clave;
-//Después de obtener la respuesta de la API, se realiza una suma de los momentos contables utilizando array_sum() en el objeto $response.
-          $sumaMomentos = array_sum((array) $response);
-          
-//El resultado de la suma se asigna a un nuevo array $response con la clave "SumaMomentos".
-          $response = ['SumaMomentos' => $sumaMomentos];
-          //print(   "response: " + $response );
+      $enero ='"S"';
+      $febrero ='"S"';
+      $marzo ='"S"';
+      $abril ='"S"';
+      $mayo ='"S"';
+      $junio ='"S"';
+      $julio ='"S"';
+      $agosto ='"S"';
+      $septiembre ='"S"';
+      $octubre ='"S"';
+      $noviembre ='"S"';
+      $diciembre ='"S"';
+
+
+
+    $body = '{
+  "Input": {
+    "Request": {
+      "Acceso": {
+        "ApiPublic":"'.$public.'",
+        "Firma":"'.$firma.'",
+        "Fecha":"'.$date.'"
+      },
+      "ConsultaDatosClaves": {
+        "TipoCvePresupuestal": "'.env('APP_SIRE_API_EJERCICIO').'" ,
+        "Periodo": '.$request->anio.',
+        "CargarSaldos": "S",
+        "CodigosClasificadores": {
+          "Clasificador1": "'.  $request->clasificador1.'",
+          "Clasificador2": "'.  $request->clasificador2.'",
+          "Clasificador3": "'.  $request->clasificador3.'",
+          "Clasificador4": "'.  $request->clasificador4.'",
+          "Clasificador5": "'.  $request->clasificador5.'",
+          "Clasificador6": "'.  $request->clasificador6.'",
+          "Clasificador7": "'.  $request->clasificador7.'",
+          "Clasificador8": "'.  $request->clasificador8.'",
+          "Clasificador9": "'.  $request->clasificador9.'",
+          "Clasificador10":"'.  $request->clasificador10.'",
+          "Clasificador11":"'.  $request->clasificador11.'"
+        },
+        "MomentosContables": {
+          "Estimado": "N",
+          "Aprobado": "S",
+          "Ampliacion": "N",
+          "Reduccion": "N",
+          "Transferencia_Aumento": "N",
+          "Transferencia_Reduccion": "N",
+          "Saldo": "N",
+          "PreComprometer": "N",
+          "Disponible": "S",
+          "Comprometido": "N",
+          "PreComprometido_Sin_Comprometer": "N",
+          "Para_PreComprometer": "N",
+          "Devengado": "S",
+          "No_Devengado": "N",
+          "PreComprometido_Sin_Deven": "N",
+          "Ejercido": "S",
+          "Recaudado": "N",
+          "Devengado_Sin_Ejerc": "N",
+          "Pagado": "S",
+          "Ejercido_Sin_Pagar": "N",
+          "PorPagar": "S"
+        },
+        "Meses": {
+          "Enero":      '.$enero.' ,
+          "Febrero":    '.$febrero.' ,
+          "Marzo":      '.$marzo.' ,
+          "Abril":      '.$abril.' ,
+          "Mayo":       '.$mayo.' ,
+          "Junio":      '.$junio.' ,
+          "Julio":      '.$julio.' ,
+          "Agosto":     '.$agosto.' ,
+          "Septiembre": '.$septiembre.' ,
+          "Octubre":    '.$octubre.' ,
+          "Noviembre":  '.$noviembre.' ,
+          "Diciembre":  '.$diciembre.'
+        }
       }
-    } catch (\Exception $e) {
-        $NUMCODE = 1;
-        $STRMESSAGE = $e->getMessage();
-        $SUCCESS = false;
     }
-    /*La función devuelve la respuesta en formato JSON, incluyendo el nuevo array $response 
-    que contiene la suma de los momentos contables correspondientes a todos los meses 
-    y al año especificado.*/
-    return response()->json([
-        'NUMCODE' => $NUMCODE,
-        'STRMESSAGE' => $STRMESSAGE,
-        'RESPONSE' => $response,
-        'SUCCESS' => $SUCCESS
-    ]);
   }
+}';
+
+
+      $client = new Client();
+      $headers = [
+                 'Content-Type' => 'application/json'
+                 ];
+       $req = new Psr7Request('POST', env('APP_SIRE_URL').'/apirest/catalogos/RConsultaClavesPresupuestales', $headers, $body);
+       $res = $client->sendAsync($req)->wait();
+       $data = json_decode($res->getBody()->getContents());
+       if($data->Result->Response->Error){
+          throw new Exception($data->Result->Response->Error);
+       }else{
+          $response= $data->Result->Response->Claves->Clave;
+
+          $totalAprobado    = 0;
+          $totalDisponible  = 0;
+          $totalDevengado   = 0;
+          $totalEjercido    = 0;
+          $totalPagado      = 0;
+          $totalPorPagar    = 0;
+          foreach ($response as &$valor) {
+
+            $totalAprobado=  $totalAprobado + $valor->Saldos->Aprobado->Total;
+            $totalDisponible=  $totalDisponible + $valor->Saldos->Disponible->Total;
+            $totalDevengado=  $totalDevengado + $valor->Saldos->Devengado->Total;
+            $totalEjercido=  $totalEjercido + $valor->Saldos->Ejercido->Total;
+            $totalPagado=  $totalPagado + $valor->Saldos->Pagado->Total;
+            $totalPorPagar=  $totalPorPagar + $valor->Saldos->PorPagar->Total;
+
+          
+        }
+
+       }
+
+
+       $responses = [
+               "P_AUTORIZADO"=>$totalAprobado, 
+               "P_MODIFICADO" =>0,
+               "P_DISPONIBLE" =>$totalDisponible,
+               "P_DEVENGADO" =>$totalDevengado,
+               "P_EJERCIDO" =>$totalEjercido,
+               "P_PAGADO" =>$totalPagado,
+               "P_PORPAGAR" =>$totalPorPagar
+       ];
+
+
+  } catch (\Exception $e) {
+      $NUMCODE = 1;
+      $STRMESSAGE = $e->getMessage();
+      $SUCCESS = false;
+  }
+
+  return response()->json(
+      [
+          'NUMCODE' => $NUMCODE,
+          'STRMESSAGE' => $STRMESSAGE,
+          'RESPONSE' => $responses,
+          'SUCCESS' => $SUCCESS
+      ]
+  );
+
+}
+
 
 }
