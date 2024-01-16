2e5b23239ee23aa9a0d334ccd6dfe46827dc148b
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2024-01-16 11:36:57 -0600

    Update SireApis.php

--- app/Http/Controllers/SireApis.php
+++ app/Http/Controllers/SireApis.php
@@ -8,11 +8,14 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request as Psr7Request;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-class SireApis extends Controller {
+
+class SireApis extends Controller
+{
 
 
 
-public function ConsultaPresupuesto(Request $request){
+  public function ConsultaPresupuesto(Request $request)
+  {
 
 
     $SUCCESS = true;
@@ -21,69 +24,69 @@ public function ConsultaPresupuesto(Request $request){
     $response = "";
     try {
 
-       // print(    $request );
+      // print(    $request );
 
-        if($request->mes ===""){
-            throw new Exception("El Parámetro mes es Obligatorio");
-        }
+      if ($request->mes === "") {
+        throw new Exception("El Parámetro mes es Obligatorio");
+      }
 
-        if($request->anio ===""){
-            throw new Exception("El Parámetro anio es Obligatorio");
-        }
+      if ($request->anio === "") {
+        throw new Exception("El Parámetro anio es Obligatorio");
+      }
 
-        $date = Carbon::now()->toDateTimeString();
-        print(    $date );
-        $enero       ='"N"';
-        $febrero     ='"N"';
-        $marzo       ='"N"';
-        $abril       ='"N"';
-        $mayo        ='"N"';
-        $junio       ='"N"';
-        $julio       ='"N"';
-        $agosto      ='"N"';
-        $septiembre  ='"N"';
-        $octubre     ='"N"';
-        $noviembre   ='"N"';
-        $diciembre   ='"N"';
+      $date = Carbon::now()->toDateTimeString();
 
-        switch ($request->mes) {
-            case 'Enero':
-                $enero ='"S"';
-                break;
-            case 'Febrero':
-                $febrero ='"S"';
-                break;
-            case 'Marzo':
-                $marzo ='"S"';
-                break;
-            case 'Abril':
-                 $abril ='"S"';
-                 break;
-            case 'Mayo':
-                 $mayo ='"S"';
-                 break;
-            case 'Junio':
-                  $junio ='"S"';
-                  break;
-            case 'Julio':
-                  $julio ='"S"';
-                  break;
-            case 'Agosto':
-                  $agosto ='"S"';
-                  break;
-            case 'Septiembre':
-                  $septiembre ='"S"';
-                  break;
-            case 'Octubre':
-                  $octubre ='"S"';
-                  break;
-            case 'Noviembre':
-                  $noviembre ='"S"';
-                  break;
-             case 'Diciembre':
-                  $diciembre ='"S"';
-                  break;
-        }
+      $enero       = '"N"';
+      $febrero     = '"N"';
+      $marzo       = '"N"';
+      $abril       = '"N"';
+      $mayo        = '"N"';
+      $junio       = '"N"';
+      $julio       = '"N"';
+      $agosto      = '"N"';
+      $septiembre  = '"N"';
+      $octubre     = '"N"';
+      $noviembre   = '"N"';
+      $diciembre   = '"N"';
+
+      switch ($request->mes) {
+        case 'Enero':
+          $enero = '"S"';
+          break;
+        case 'Febrero':
+          $febrero = '"S"';
+          break;
+        case 'Marzo':
+          $marzo = '"S"';
+          break;
+        case 'Abril':
+          $abril = '"S"';
+          break;
+        case 'Mayo':
+          $mayo = '"S"';
+          break;
+        case 'Junio':
+          $junio = '"S"';
+          break;
+        case 'Julio':
+          $julio = '"S"';
+          break;
+        case 'Agosto':
+          $agosto = '"S"';
+          break;
+        case 'Septiembre':
+          $septiembre = '"S"';
+          break;
+        case 'Octubre':
+          $octubre = '"S"';
+          break;
+        case 'Noviembre':
+          $noviembre = '"S"';
+          break;
+        case 'Diciembre':
+          $diciembre = '"S"';
+          break;
+      }
 
 
 
@@ -93,24 +96,24 @@ public function ConsultaPresupuesto(Request $request){
         "Acceso": {
           "ApiPublic": "HIRMZQRKWB",
           "Firma": "297E6DBA0A3428F60A68EBBBDAE4D3E310342E2A",
-          "Fecha": '.$date.'
+          "Fecha": ' . $date . '
         },
         "ConsultaDatosClaves": {
           "TipoCvePresupuestal": "E6",
-          "Periodo": '.$request->anio.',
+          "Periodo": ' . $request->anio . ',
           "CargarSaldos": "S",
           "CodigosClasificadores": {
-            "Clasificador1": "'.  $request->clasificador1.'",
-            "Clasificador2": "'.  $request->clasificador2.'",
-            "Clasificador3": "'.  $request->clasificador3.'",
-            "Clasificador4": "'.  $request->clasificador4.'",
-            "Clasificador5": "'.  $request->clasificador5.'",
-            "Clasificador6": "'.  $request->clasificador6.'",
-            "Clasificador7": "'.  $request->clasificador7.'",
-            "Clasificador8": "'.  $request->clasificador8.'",
-            "Clasificador9": "'.  $request->clasificador9.'",
-            "Clasificador10":"'.  $request->clasificador10.'",
-            "Clasificador11":"'.  $request->clasificador11.'"
+            "Clasificador1": "' .  $request->clasificador1 . '",
+            "Clasificador2": "' .  $request->clasificador2 . '",
+            "Clasificador3": "' .  $request->clasificador3 . '",
+            "Clasificador4": "' .  $request->clasificador4 . '",
+            "Clasificador5": "' .  $request->clasificador5 . '",
+            "Clasificador6": "' .  $request->clasificador6 . '",
+            "Clasificador7": "' .  $request->clasificador7 . '",
+            "Clasificador8": "' .  $request->clasificador8 . '",
+            "Clasificador9": "' .  $request->clasificador9 . '",
+            "Clasificador10":"' .  $request->clasificador10 . '",
+            "Clasificador11":"' .  $request->clasificador11 . '"
           },
           "MomentosContables": {
             "Estimado": "N",
@@ -136,18 +139,18 @@ public function ConsultaPresupuesto(Request $request){
             "PorPagar": "N"
           },
           "Meses": {
-            "Enero":      '.$enero.' ,
-            "Febrero":    '.$febrero.' ,
-            "Marzo":      '.$marzo.' ,
-            "Abril":      '.$abril.' ,
-            "Mayo":       '.$mayo.' ,
-            "Junio":      '.$junio.' ,
-            "Julio":      '.$julio.' ,
-            "Agosto":     '.$agosto.' ,
-            "Septiembre": '.$septiembre.' ,
-            "Octubre":    '.$octubre.' ,
-            "Noviembre":  '.$noviembre.' ,
-            "Diciembre":  '.$diciembre.'
+            "Enero":      ' . $enero . ' ,
+            "Febrero":    ' . $febrero . ' ,
+            "Marzo":      ' . $marzo . ' ,
+            "Abril":      ' . $abril . ' ,
+            "Mayo":       ' . $mayo . ' ,
+            "Junio":      ' . $junio . ' ,
+            "Julio":      ' . $julio . ' ,
+            "Agosto":     ' . $agosto . ' ,
+            "Septiembre": ' . $septiembre . ' ,
+            "Octubre":    ' . $octubre . ' ,
+            "Noviembre":  ' . $noviembre . ' ,
+            "Diciembre":  ' . $diciembre . '
           }
         }
       }
@@ -156,38 +159,49 @@ public function ConsultaPresupuesto(Request $request){
 
 
 
-       $client = new Client();
-        $headers = [
-                   'Content-Type' => 'application/json'
-                   ];
 
+      print($body);
 
+      $client = new Client();
+      $headers = [
+        'Content-Type' => 'application/json'
+      ];
 
-         $req = new Psr7Request('POST', env('APP_SIRE_URL').'/apirest/catalogos/RConsultaClavesPresupuestales', $headers, $body);
-         $res = $client->sendAsync($req)->wait();
-         $data = json_decode($res->getBody()->getContents());
-         $response =  $data;
+      $req = new Psr7Request('POST', env('APP_SIRE_URL') . '/apirest/catalogos/RConsultaClavesPresupuestales', $headers, $body);
 
+      $res = $client->sendAsync($req)->wait();
+
+      // Verifica si la solicitud fue exitosa (código de estado 2xx)
+      if ($res->getStatusCode() >= 200 && $res->getStatusCode() < 300) {
+        $responseBody = $res->getBody()->getContents();
+        print($responseBody);
+        $data = json_decode($responseBody);
+        print($data);
+        $response = $data;
+      } else {
+        // Puedes manejar otros códigos de estado aquí si es necesario
+        echo "Error en la solicitud HTTP. Código de estado: " . $res->getStatusCode();
+        throw new Exception("Error en la solicitud HTTP. Código de estado: " . $res->getStatusCode());
+      }
     } catch (\Exception $e) {
-        $NUMCODE = 1;
-        $STRMESSAGE = $e->getMessage();
-        $SUCCESS = false;
+      $NUMCODE = 1;
+      $STRMESSAGE = $e->getMessage();
+      $SUCCESS = false;
     }
 
     return response()->json(
-        [
-            'NUMCODE' => $NUMCODE,
-            'STRMESSAGE' => $STRMESSAGE,
-            'RESPONSE' => $response,
-            'SUCCESS' => $SUCCESS
-        ]
+      [
+        'NUMCODE' => $NUMCODE,
+        'STRMESSAGE' => $STRMESSAGE,
+        'RESPONSE' => $response,
+        'SUCCESS' => $SUCCESS
+      ]
     );
+  }
 
-}
 
-
-public function SelectIndex(Request $request)
-{
+  public function SelectIndex(Request $request)
+  {
 
 
     $SUCCESS = true;
@@ -196,24 +210,24 @@ public function SelectIndex(Request $request)
     $response = "";
 
     try {
-        $response ="EJEMPLO DE RESPUESTA";
-        /*
+      $response = "EJEMPLO DE RESPUESTA";
+      /*
         $response = DB::select("SELECT  rm.*,rl.Menu menupadre  FROM  REPCENTRAL.Menus rm
         LEFT JOIN REPCENTRAL.Menus rl ON rm.MenuPadre = rl.id
         WHERE rm.deleted=0 ORDER BY rm.nivel ");*/
     } catch (\Exception $e) {
-        $NUMCODE = 1;
-        $STRMESSAGE = $e->getMessage();
-        $SUCCESS = false;
+      $NUMCODE = 1;
+      $STRMESSAGE = $e->getMessage();
+      $SUCCESS = false;
     }
 
     return response()->json(
-        [
-            'NUMCODE' => $NUMCODE,
-            'STRMESSAGE' => $STRMESSAGE,
-            'RESPONSE' => $response,
-            'SUCCESS' => $SUCCESS
-        ]
+      [
+        'NUMCODE' => $NUMCODE,
+        'STRMESSAGE' => $STRMESSAGE,
+        'RESPONSE' => $response,
+        'SUCCESS' => $SUCCESS
+      ]
     );
-}
+  }
 }
